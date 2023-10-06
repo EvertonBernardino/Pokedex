c601d88709ffe86735dd5a680eeac36aea535118
Everton Bernardino <ceverto11@gmail.com>
2023-10-06 07:02:32 -0300

    atualização

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,25 @@
-console.log("Funcionando!")
+
+
+function convertPokemonTohtml(pokemon){
+return `
+        <li class="pokemon">
+            <span class="number">#001</span>
+            <span class="name">${pokemon.name}</span>
+
+            <div class="detail">
+                <ol class="types">
+                    <li class="types">grass</li>
+                    <li class="types">poison</li>
+                </ol>
+                <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg" alt="${pokemon.name}">
+            </div>                
+        </li>
+    `
+}
+
+const pokemonList = document.getElementById(`pokemonList`)
+
+pokeAPI.getPokemons().then((pokemons = []) => {
+    pokemonList.innerHTML += pokemons.map(convertPokemonTohtml).join(``)   
+})
+
---
+++ assets/js/poke-api.js
@@ -0,0 +1,9 @@
+const pokeAPI = {}
+
+pokeAPI.getPokemons = (offset = 0, limit =10 ) => {
+    const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
+    return fetch(url)
+        .then((response) => response.json())
+        .then((jsonBody) => jsonBody.results)
+        .catch ((error) => console.error(error))
+}
